f654a3b257af263ac317d0d7bcae644678d1dd47
leamilhiet <99493437+leamilhiet@users.noreply.github.com>
2022-02-14 02:06:07 +0100

    Delete qgis2web_2022_02_14-01_19_28_187797 directory

--- qgis2web_2022_02_14-01_19_28_187797/layers/Atelierdetransformation_2.js
+++
@@ -1,1 +0,0 @@
-var json_Atelierdetransformation_2 = {"type":"FeatureCollection","name":"Atelierdetransformation_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"communes":"BOU","longitude":2.04071,"latitude":47.86089,"nom":"Atelier de transformation"},"geometry":{"type":"Point","coordinates":[2.040712,47.860893]}}]}
